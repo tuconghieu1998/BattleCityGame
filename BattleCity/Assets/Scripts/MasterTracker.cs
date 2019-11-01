d1a3e73c9f744661020d725160cea76866a503a0
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-11-01 23:07:38 +0700

    Init project, create tilemaps: ground, water,.., create tank, handling Tank Movement: Player and EnemyAI tank

---
+++ BattleCity/Assets/Scripts/MasterTracker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterTracker : MonoBehaviour
+{
+    static MasterTracker instance = null;
+
+    [SerializeField]
+    int smallTankPoints = 100, fastTankPoints = 200, bigTankPoints = 300, armoredTankPoints = 400;
+    public int smallTankPointsWorth { get { return smallTankPoints; } }
+    public int fastTankPointsWorth { get { return fastTankPoints; } }
+    public int bigTankPointsWorth { get { return bigTankPoints; } }
+    public int armoredTankPointsWorth { get { return armoredTankPoints; } }
+
+    public static int smallTankDestroyed, fastTankDestroyed, bigTankDestroyed, armoredTankDestroyed;
+    public static int stageNumber;
+    public static int playerScore = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BattleCity/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Movement
+{
+    Rigidbody2D rb2d;
+    float h, v;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (h != 0 && !isMoving) StartCoroutine(MoveHorizontal(h, rb2d));
+        else if (v != 0 && !isMoving) StartCoroutine(MoveVertical(v, rb2d));
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+}
